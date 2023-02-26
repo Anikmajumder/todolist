2b7544f03fcd5170a0ffc69e7a7e8f27090325db
Anikmajumder <53091562+Anikmajumder@users.noreply.github.com>
2023-02-26 21:05:05 +0600

    user sign up added

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import TaskList,TaskDetail,TaskCreate, TaskUpdate, TaskDelete,CustomLoginView
+from .views import TaskList,TaskDetail,TaskCreate, TaskUpdate, TaskDelete,CustomLoginView,SignUpPage
 from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
    path('login/',CustomLoginView.as_view(),name = 'login'),
    path('logout/',LogoutView.as_view(next_page='login'),name = 'logout'),
+   path('sign_up/',SignUpPage.as_view(),name='sign_up'),
+
    path('', TaskList.as_view(),name='tasks'),
    path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
    path('task-create/',TaskCreate.as_view(),name='task-create'),
